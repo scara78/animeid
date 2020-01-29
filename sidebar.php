4327a59fda2fc709bf8a577449c22b2a7ae234e1
shigatsuid <shigatsuid@gmail.com>
2020-01-29 23:54:40 +0700

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,35 +1,35 @@
 <div class="col-lg-4 col-12">
 <div class="homepage">
 <div class="colom-1">
-<h4 class="judul_popular_week">Populer Drama</h4>
+<h4 class="judul_popular_week">Populer Anime</h4>
 <hr>
 <div class="popular-versi-desktop">
 <div class="box-body">
 <div class="content">
 <ul class="collection">
 <li class="list-item">
-<a href="/survival-wedding/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/r/160x220/images/anime/1068/97169.webp?s=7d3ec07dbc8f65cb891ec1b72b4252aa" alt="Survival Wedding">
+<a href="https://animeidme.herokuapp.com/search.php?title=piece">
+<img class="popular-forever-image" src="https://cdnimg.xyz/images/anime/One-piece.jpg" alt="One Piece">
 </a>
 </li>
 <li class="list-item">
-<a href="/good-doctor/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/r/160x220/images/anime/1125/96929.webp?s=7d1859de706ef7b5b3168ea6c794d692" alt="Good Doctor">
+<a href="https://animeidme.herokuapp.com/search.php?title=boruto">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/boruto-naruto-next-generations.png" alt="Boruto">
 </a>
 </li>
 <li class="list-item">
-<a href="/cheer-dance/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/r/160x220/images/anime/1021/95670.webp?s=feffa91a7e46f7b41bca1e4301871de5" alt="Cheer Dance">
+<a href="https://animeidme.herokuapp.com/search.php?title=Haikyuu!!">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/haikyuu-to-the-top.png" alt="Haikyuu S3">
 </a>
 </li>
 <li class="list-item">
-<a href="/code-blue-mou-hitotsu-no-nichijou/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/r/160x220/images/anime/1933/97061.webp?s=0061716af400d369c4c27108639cf5b0" alt="Code Blue: Mou Hitotsu no Nichijou">
+<a href="https://animeidme.herokuapp.com/itai-no-wa-iya-nano-de-bougyoryoku-ni-kyokufuri-shitai-to-omoimasu/156715/4">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/itai-no-wa-iya-nano-de-bougyoryoku-ni-kyokufuri-shitai-to-omoimasu.png" alt="Itai no Wa Iyaaaaa">
 </a>
 </li>
 <li class="list-item">
-<a href="/ani-ni-aisaresugite-komattemasu/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/r/160x220/images/anime/1694/93337.webp?s=3acc734515975ae4845384cfa9a3f1e1" alt="Ani ni Aisaresugite Komattemasu">
+<a href="https://animeidme.herokuapp.com/somali-to-mori-no-kamisama/152172/3">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/somali-to-mori-no-kamisama.png" alt="Somali to Mori no Kamisama">
 </a>
 </li>
 </ul>
@@ -42,49 +42,44 @@
 <div class="content">
 <ul class="collection">
 <li class="list-item">
-<a href="/chihayafuru-musubi/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/images/anime/5/87048.jpg" alt="Chihayafuru: Musubi">
+<a href="https://animeidme.herokuapp.com/kimi-no-na-wa/2581/1">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/kimi-no-na-wa.jpg" alt="Kimi no Na wa">
 </a>
 </li>
 <li class="list-item">
-<a href="/itazurana-kiss-the-movie-campus/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/images/anime/1122/96435.jpg" alt="Itazurana Kiss The Movie: Campus">
+<a href="https://animeidme.herokuapp.com/koe-no-katachi/2647/1">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/koe-no-katachi-movie.png" alt="Koe no Katachi">
 </a>
 </li>
 <li class="list-item">
-<a href="/azumi-haruko-wa-yukuefumei/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/images/anime/10/51723.jpg" alt="Azumi Haruko wa Yukuefumei">
+<a href="https://animeidme.herokuapp.com/seishun-buta-yarou-wa-yumemiru-shoujo-no-yume-wo-minai/142016/1">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/seishun-buta-yarou-wa-yumemiru-shoujo-no-yume-wo-minai.png" alt="Seishun Buta Yarou">
 </a>
 </li>
 <li class="list-item">
-<a href="/nagasaki-memories-of-my-son/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/images/anime/8/82911.jpg" alt="Nagasaki: Memories of My Son">
+<a href="https://animeidme.herokuapp.com/youjo-senki-movie/130865/1">
+<img class="popular-forever-image" src="https://cdnimg.xyz/cover/youjo-senki-movie.png" alt="Youjo Senki">
 </a>
 </li>
 <li class="list-item">
-<a href="/nigakute-amai/">
-<img class="popular-forever-image" src="https://cdn.myanimelist.net/images/anime/11/89556.jpg" alt="Nigakute Amai">
+<a href="https://animeidme.herokuapp.com/omoide-no-marnie/3553/1">
+<img class="popular-forever-image" src="https://cdnimg.xyz/images/upload/64293.jpg" alt="Omoide no Marnie">
 </a>
 </li>
 </ul>
 </div>
 <div class="title-bottom">
-<span class="font-weight-bold">Top 5 Movie Anime</span>
+<span class="font-weight-bold">Ongoing Anime</span>
 </div>
 </div>
 </div>
 <div class="popular-versi-smartphone">
 <ul>
-<li><a href="/survival-wedding/">Survival Wedding</a></li>
-<li><a href="/good-doctor/">Good Doctor</a></li>
-<li><a href="/cheer-dance/">Cheer Dance</a></li>
-<li><a href="/code-blue-mou-hitotsu-no-nichijou/">Code Blue: Mou Hitotsu no Nichijou</a></li>
-<li><a href="/ani-ni-aisaresugite-komattemasu/">Ani ni Aisaresugite Komattemasu</a></li>
-<li><a href="/chihayafuru-musubi/">Chihayafuru: Musubi</a></li>
-<li><a href="/gibo-to-musume-no-blues/">Gibo to Musume no Blues</a></li>
-<li><a href="/takane-no-hana/">Takane no Hana</a></li>
-<li><a href="/giver-fukushu-no-zoyosha/">Giver: Fukushu no Zoyosha</a></li>
-<li><a href="/itazurana-kiss-the-movie-campus/">Itazurana Kiss The Movie: Campus</a></li>
+<li><a href="https://animeidme.herokuapp.com/haikyuu-to-the-top/156765/3">Haikyuu!!: To The Top</a></li>
+<li><a href="https://animeidme.herokuapp.com/somali-to-mori-no-kamisama/152172/3">Somali to Mori no Kamisama</a></li>
+<li><a href="https://animeidme.herokuapp.com/search.php?title=piece">One Piece</a></li>
+<li><a href="https://animeidme.herokuapp.com/search.php?title=boruto">Boruto</a></li>
+<li><a href="https://animeidme.herokuapp.com/search.php?title=boku+no+hero">Boku no Hero Academia</a></li>
 </ul>
 </div>
 </div>
